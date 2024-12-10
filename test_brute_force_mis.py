6acc4e008dc8c153945a397a3a4093a66f0fce82
Alexandre-Jz <alexandre.jrz.83@gmail.com>
2024-12-10 10:45:05 +0100

    test_brute_force_mis.py

--- test_brute_force_mis.py
+++ test_brute_force_mis.py
@@ -1,19 +1,39 @@
 from brute_force_MIS import brute_force_mis
 
-G = {
-    "a": ["s", "e", "c", "b"],
-    "b": ["a", "c", "f"],
-    "c": ["a", "b", "f", "e", "d"],
-    "d": ["e", "c", "f"],
-    "e": ["s", "a", "c", "d"],
-    "f": ["d", "c", "b"],
-    "s": ["a", "e"],
-}
 
-def test_brute_force_MIS():
+def test_brute_force_MIS_1():
+    G = {
+        "a": ["s", "e", "c", "b"],
+        "b": ["a", "c", "f"],
+        "c": ["a", "b", "f", "e", "d"],
+        "d": ["e", "c", "f"],
+        "e": ["s", "a", "c", "d"],
+        "f": ["d", "c", "b"],
+        "s": ["a", "e"],
+    }
+
     set_test = set()
 
-    for i in brute_force_mis(G) :
+    for i in brute_force_mis(G):
         set_test.add(i)
 
-    assert set_test == set({'s','b','d'})
+    assert set_test == set({"s", "b", "d"})
+
+
+def test_brute_force_MIS_2():
+    G = {
+        "a": ["s", "e", "c", "b"],
+        "b": ["a", "c", "f"],
+        "c": ["a", "b", "f", "e", "d"],
+        "d": ["e", "c", "f", "s"],
+        "e": ["s", "a", "c", "d"],
+        "f": ["d", "c", "b"],
+        "s": ["a", "e", "d"],
+    }
+
+    set_test = set()
+
+    for i in brute_force_mis(G):
+        set_test.add(i)
+
+    assert set_test == set({"s", "b"})
