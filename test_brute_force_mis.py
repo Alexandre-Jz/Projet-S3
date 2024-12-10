200fec10346d25110394ecef4b27a9688ba0ecd0
Alexandre-Jz <alexandre.jrz.83@gmail.com>
2024-12-10 11:06:31 +0100

    Update test_brute_force_mis.py

--- test_brute_force_mis.py
+++ test_brute_force_mis.py
@@ -18,22 +18,3 @@ def test_brute_force_MIS_1():
         set_test.add(i)
 
     assert set_test == set({"s", "b", "d"})
-
-
-def test_brute_force_MIS_2():
-    G = {
-        "a": ["s", "e", "c", "b"],
-        "b": ["a", "c", "f"],
-        "c": ["a", "b", "f", "e", "d"],
-        "d": ["e", "c", "f", "s"],
-        "e": ["s", "a", "c", "d"],
-        "f": ["d", "c", "b"],
-        "s": ["a", "e", "d"],
-    }
-
-    set_test = set()
-
-    for i in brute_force_mis(G):
-        set_test.add(i)
-
-    assert set_test == set({"s", "b"})
